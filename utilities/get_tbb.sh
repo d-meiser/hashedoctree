32fd8d1e670bbba5c6b6c9054229bd59d2a1a74f
Dominic Meiser <dmeiser79@gmail.com>
2017-12-13 20:55:19 -0700

    First cut at script to download tbb

---
+++ utilities/get_tbb.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ ! -e 2018_U2.tar.gz ];
+then
+  wget https://github.com/01org/tbb/archive/2018_U2.tar.gz
+fi
+if [ ! -e tbb-2018_U2 ];
+then
+  tar xf 2018_U2.tar.gz
+fi
+cd tbb-2018_U2
+make -j4
+sh build/generate_tbbvars.sh
+source tbbvars.sh
+cd -
+
