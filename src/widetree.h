55e89fee8ef189dab414ab45f687d60a2eb9ec87
Dominic Meiser <dominic@sketchup.com>
2017-12-21 17:11:37 -0700

    Add sorting to counting_sort_test

--- src/widetree.h
+++ src/widetree.h
@@ -5,10 +5,7 @@
 
 uint8_t ComputeWideKey(HOTBoundingBox bbox, HOTPoint location);
 
-void SortByKey(
-    const uint8_t* key_begin, const uint8_t* key_end,
-    const int* perm_in,
-    int* perm_out);
+void SortByKey(const uint8_t* keys, int n, int* perm);
 
 
 #endif
--- tests/counting_sort_test.cpp
+++ tests/counting_sort_test.cpp
@@ -5,8 +5,16 @@
 #include <numeric>
 
 
+template <typename T>
+void ApplyPermutation(const int* perm, int n, const T* in, T* out) {
+  for (int i = 0; i < n; ++i) {
+    out[perm[i]] = in[i];
+  }
+}
+
+
 int main(int, char **) {
-  int n = 100000;
+  int n = 1000000;
   uint64_t start, end;
 
   HOTBoundingBox bbox = unit_cube();
@@ -22,7 +30,15 @@ int main(int, char **) {
   std::cout << "Generate keys: " << (end - start) / 1.0e6 << std::endl;
 
   std::vector<int> perm(n);
-  std::iota(perm.begin(), perm.end(), 0);
+  start = rdtsc();
+  SortByKey(&keys[0], n, &perm[0]);
+  end = rdtsc();
+  std::cout << "SortByKey: " << (end - start) / 1.0e6 << std::endl;
 
+  std::vector<HOTItem> sorted_items(n);
+  start = rdtsc();
+  ApplyPermutation(&perm[0], n, &items[0], &sorted_items[0]);
+  end = rdtsc();
+  std::cout << "ApplyPermutation: " << (end - start) / 1.0e6 << std::endl;
 }
 
